1683e8b8eaa41c382aacaf6e9dc9c00f1fd99d19
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-03-28 17:50:04 +0300

    fix logo url

--- components/rak-tlv.tsx
+++ components/rak-tlv.tsx
@@ -25,7 +25,7 @@ const RakTLVLogo = () => {
         );
     }
 
-    const logoURL = `https://dxohrymifwxwyppgvmff.supabase.co/storage/v1/object/public/assets//logo-light.png`
+    const logoURL = `https://dxohrymifwxwyppgvmff.supabase.co/storage/v1/object/public/assets//logo.png`
     const lightLogoURL = `https://dxohrymifwxwyppgvmff.supabase.co/storage/v1/object/public/assets//logo-light.png`
     const logosrc = theme === 'light' ? lightLogoURL : logoURL;
     // const logosrc = `/assets/logos/png/1x/logo.png`;
